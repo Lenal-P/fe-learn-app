185857816d1baa03e074139dcc3bb9c5883720ba
Lenal-P <lenalpiggy@gmail.com>
2024-09-09 09:20:03 +0700

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>Hello world
+      <div>
+        <Button type="primary">New</Button>
+      </div>
+    </div>
+  );
+}
